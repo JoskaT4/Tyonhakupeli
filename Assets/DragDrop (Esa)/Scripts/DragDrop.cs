dd02e7b7b955ec831665562ddb287626bdf8a22b
KoodiKalastaja <esa.soininen@outlook.com>
2024-11-20 09:31:04 +0200

    Yleisiä muutoksia
    Drag&Droppia jatkoin
    Yritän lisätä toiminnon, joka instantiate-toiminnolla luo CV/Työhaun (kesken)
    Lisäsin uuden tagin nimeltä "Desktop"

--- Assets/DragDrop (Esa)/Scripts/DragDrop.cs
+++ Assets/DragDrop (Esa)/Scripts/DragDrop.cs
@@ -12,29 +12,34 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
 
-    private void Awake() {
+    private void Awake() 
+    {
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    public void OnBeginDrag(PointerEventData eventData) {
+    public void OnBeginDrag(PointerEventData eventData) 
+    {
         Debug.Log("OnBeginDrag");
         canvasGroup.alpha = .6f;
         canvasGroup.blocksRaycasts = false;
     }
 
-    public void OnDrag(PointerEventData eventData) {
+    public void OnDrag(PointerEventData eventData) 
+    {
         //Debug.Log("OnDrag");
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
-    public void OnEndDrag(PointerEventData eventData) {
+    public void OnEndDrag(PointerEventData eventData) 
+    {
         Debug.Log("OnEndDrag");
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
     }
 
-    public void OnPointerDown(PointerEventData eventData) {
+    public void OnPointerDown(PointerEventData eventData) 
+    {
         Debug.Log("OnPointerDown");
     }
 
