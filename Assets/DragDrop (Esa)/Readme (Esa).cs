678a44fa370bb5131c02807d39cccb28c21ae8fa
KoodiKalastaja <esa.soininen@outlook.com>
2024-11-26 14:33:01 +0200

    Readme
    Tein vain itselleni Readme:n jotta muistan jotkut asiat. Tarvittaessa laitan näitä enemmän DragDrop:iin muille luettavaksi ohjeiksi, jos olen poissa.

---
+++ Assets/DragDrop (Esa)/Readme (Esa).cs
@@ -0,0 +1,27 @@
+//                  Työantajan vaatimuksia:
+// - Työturvallisuuskortti
+// - Hyvä fyysinen kunto
+// - Valmius kulkea työn perässä tai asuu lähellä
+// - Kokemus?
+// - Motivaatio näkyy haussa?
+// - Tiimitaidot?
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
