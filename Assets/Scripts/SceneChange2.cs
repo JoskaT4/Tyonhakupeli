a0c6a09da777227c8c8627acb0d7b9dfa47b7dc4
JoskaT4 <josetolppala1@gmail.com>
2024-11-28 11:21:16 +0200

    Mailin säätöä

---
+++ Assets/Scripts/SceneChange2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SceneChange2 : MonoBehaviour
+{
+   
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+
+
+
+
+
+
+
+
+}
