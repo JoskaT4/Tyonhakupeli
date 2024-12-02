bba56de4ab9b4e735e33e7782a9e7632f9a3b93f
KoodiKalastaja <esa.soininen@outlook.com>
2024-12-02 12:31:17 +0200

    Mailin skenevaihto ja yksi bugikorjaus.
    Mailiin pääsee nyt tuplaklikkauksella. Scriptissä nyt klikkauksella voi aktivoida napin, mutta jos haluaa tuplaklikkauksella, otat napin pois käytöstä inspektorissa.
    
    Näiden lisäksi korjasin bugin, joka aiheutti sen, että ruksaus ei lähtenyt pois, kun CVslotista poistin CV:n.
    Ja olen alkanut tekemään pisteyksien alkuja.

--- Assets/Scripts/SceneChange2.cs
+++ Assets/Scripts/SceneChange2.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement; 
 
-public class SceneChange2 : MonoBehaviour
+public class SceneChange2: MonoBehaviour, IPointerClickHandler
 {
-   
-    public void ChangeScene()
+    int tap;
+
+
+    public void OnPointerClick(PointerEventData eventData)
     {
-        SceneManager.LoadScene(6);
+        tap = eventData.clickCount;
+        if (tap == 2)
+        {
+            SceneManager.LoadScene(6); // When button is unchecked in Inspector, only a double click will activate this method.  
+        }
     }
 
-
-
-
-
-
-
-
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(6); // When button is checked in Inspector, single click will activate this method.
+    }
 
 }
