c06280dd76d6ce768cc2287f2087be7ae25600f0
jonttukala <155264285+jonttukala@users.noreply.github.com>
2024-12-02 12:53:57 +0200

    sound

---
+++ Assets/Scripts/SceneChange4.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange4 : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(9);
+    }
+}
