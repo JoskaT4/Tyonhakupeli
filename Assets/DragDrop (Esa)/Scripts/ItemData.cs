78d43638f81633878b9ada8de7532f097b86973b
Essillore <riikka.pehkonen@me.com>
2024-12-04 13:12:54 +0200

    Reapply "Pisteytyssysteemiin päivitys"
    This reverts commit 98432fb738153fc1fb083d3edc86666f91f45c26.

---
+++ Assets/DragDrop (Esa)/Scripts/ItemData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : MonoBehaviour
+{   
+    public int itemScore; // Assign this value in the Unity Inspector
+    
+}
