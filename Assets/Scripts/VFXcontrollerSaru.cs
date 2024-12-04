8e6c6184c99ac4565aef10b34b7d1248705d3813
Sardiini <saru.halla@gmail.com>
2024-12-04 12:58:16 +0200

    VFX implementointi
    kursorin latausanimaatio toimii nyt oikein klikatessa ja se pitää enää yhydistää niihin paikkoihin mihin tarvitsee

--- Assets/Scripts/VFXcontrollerSaru.cs
+++ Assets/Scripts/VFXcontrollerSaru.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class VFXcontrollerSaru : MonoBehaviour
 {
     public ParticleSystem lataus;
+    public Vector3 offset = new Vector3(-0.01f, -0.01f, 0f);
 
     // Start is called before the first frame update
     void Start()
@@ -15,24 +16,23 @@ public class VFXcontrollerSaru : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        Vector3 cursorPos = Input.mousePosition;
+        cursorPos.z = Camera.main.nearClipPlane;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(cursorPos);
+        lataus.transform.position = worldPos + offset;
 
-    private void OnMouseDown()
-    {
-       
-        if (lataus != null) {
+        // Play the particle system on mouse click
+        if (Input.GetMouseButtonDown(0)) // 0 = Left mouse button
+        {
+            // Get the cursor position in screen space and convert it to world space
+           
 
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out RaycastHit hit)) {
-
-                ParticleSystem effect = Instantiate(lataus, transform.position, Quaternion.identity);
-                effect.Play();
-                Destroy(effect.gameObject, effect.main.duration + effect.main.startLifetime.constantMax);
-
-
-            }
+            // Move the particle effect to the cursor position
+            
+            ParticleSystem newParticle = Instantiate(lataus, worldPos, Quaternion.identity);
+            lataus.Play();
+            Destroy(newParticle.gameObject, newParticle.main.duration);
         }
     }
+   
 }
